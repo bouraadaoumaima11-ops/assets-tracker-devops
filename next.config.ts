0ba63d16ff6457614ff9d4ec609328d31c61b48a
OUMAIMA_BOURAADA <bouraadaoumaima11@gmail.com>
2026-08-30 14:00:04 +0100

    fix: remove turbopack from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -67,7 +67,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [{ protocol: "https", hostname: "lh3.googleusercontent.com" }],
   },
   experimental: {
-    turbopack: false,  // ← AJOUTER CETTE LIGNE POUR DÉSACTIVER TURBOPACK
     viewTransition: true,
     optimizePackageImports: [
       "recharts",
